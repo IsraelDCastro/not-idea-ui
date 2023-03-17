ff3969c3a6dac0e84b0075111e4deca5c189fc08
Israel De Castro A <fulanito203@gmail.com>
2023-03-17 14:51:16 -0400

    Description added to inputs, input with datalist added, and some fixed.

---
+++ index.d.ts
@@ -0,0 +1,6 @@
+declare module "not-idea-ui" {
+  export { default as default } from "./main.scss";
+  export { default as tailwind } from "./tailwind/not-idea-ui.css";
+  export { default as css } from "./css/not-idea-ui.css";
+  export { default as cssMin } from "./css/not-idea-ui.min.css";
+}
