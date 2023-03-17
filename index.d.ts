b2bbd28534a47cd261426f879f7c8f94a3f0ad8d
Israel De Castro A <fulanito203@gmail.com>
2023-03-17 15:07:07 -0400

    Little fix on package.json

--- index.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "not-idea-ui" {
-  export { default as default } from "./main.scss";
-  export { default as tailwind } from "./tailwind/not-idea-ui.css";
-  export { default as css } from "./css/not-idea-ui.css";
-  export { default as cssMin } from "./css/not-idea-ui.min.css";
-}
